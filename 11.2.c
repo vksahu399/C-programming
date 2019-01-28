d59f929f91c65d1bfa8f886ffd5746be86b44a32
VIVEK KUMAR SAHU <vksahu399@gmail.com>
2019-01-29 01:00:19 +0530

    update

--- 11.2.c
+++ 11.2.c
@@ -1,14 +1,14 @@
 # include<stdio.h>
 void main()
 {
-	int num,sum=0,a,i;
+	int num,sum=0,a;
 	printf("enter the num");
 	scanf("%d",&num);
-    for(i=1;i<=num*10;i++)
-    {
+	while(num>0)
+	{
 		a=num%10;
 		sum=sum*10+a;
 		num=num/10;
-    }
-		printf("%d",sum);
+    	}
+	printf("%d",sum);
 }
